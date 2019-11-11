ca0d0ba05e3af773055313045a4856c2ee45370b
Luis S <lsanch10@uncc.edu>
2019-11-11 00:49:48 +0000

    Complete Create and Show.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,8 +1,18 @@
 class ClientsController < ApplicationController
+    def show
+        @client = Client.find(params[:id])
+    end
     def new
     end
     
     def create
-        render plain: params[:client].inspect
+        @client = Client.new(article_params)
+        
+        @client.save
+        redirect_to @client
     end
+end
+private 
+def article_params
+        params.require(:client).permit(:Name, :Address, :Phone_Number, :Email, :Court_Date, :Case_Description)
 end
