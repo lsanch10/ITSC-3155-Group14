fe7a26fa8e601a13a79a7069f5bc7a4615f5f1c9
cvogel6 <cvogel6@uncc.edu>
2019-11-25 03:21:57 +0000

    Added second scenario

--- features/step_definitions/client_steps.rb
+++ features/step_definitions/client_steps.rb
@@ -6,8 +6,17 @@ When("I click on the {string} link") do |string|
   click_link string
 end
 
+Given("I am on the Client List page") do
+  visit clients_path
+end
 
+Then("I should be on the {string} page") do |string|
+  expect(page).to have_content(string)
+end
 
+Then("I should see the {string} field") do |string|
+  expect(page).to have_field(string)
+end
 
 
 
