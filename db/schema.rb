2c6120e0f8eb0624a88c3fcbc39f7d69feb52970
Luis <Lsanch10@uncc.edu>
2019-12-01 22:57:20 +0000

    removed all files associated to bill

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_11_17_233001) do
 
-  create_table "bills", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "date"
-    t.text "description"
-    t.string "rate"
-    t.string "hours"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  
 
   create_table "clients", force: :cascade do |t|
     t.string "Name"
