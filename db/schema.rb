6c2b4fd75789f8cd347f840b84a64c2533543870
Luis <Lsanch10@uncc.edu>
2019-12-01 23:11:24 +0000

    updated apps db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_11_30_010656) do
 
-
-  
-
   create_table "bills", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -28,13 +25,12 @@ ActiveRecord::Schema.define(version: 2019_11_30_010656) do
     t.index ["client_id"], name: "index_bills_on_client_id"
   end
 
-
   create_table "clients", force: :cascade do |t|
     t.string "Name"
     t.string "Address"
     t.string "Phone_Number"
     t.string "Email"
-    t.string "Court_Date"
+    t.date "Court_Date"
     t.text "Case_Description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
