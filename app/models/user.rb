8be49a0ca51ccae75ca988dcc5634fe6bc4b1a9d
Luis S <lsanch10@uncc.edu>
2019-12-12 00:17:10 +0000

    added css to views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :clients
   
   VALID_EMAIL_REGEX = /A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    before_save { self.email = email.downcase }
+    before_save { self.email = email.downcase}
     
     validates :name,
         presence: true,
